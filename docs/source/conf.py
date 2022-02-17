26e18f18ce936ae19c29f32fc5974d6b24d250f8
Zhibo Huang <zhibo90@126.com>
2022-02-17 21:23:59 +0800

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = '2022, Huang Zhibo'
 author = 'Huang Zhibo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3'
+release = '0.5.2'
 
 
 # -- General configuration ---------------------------------------------------
